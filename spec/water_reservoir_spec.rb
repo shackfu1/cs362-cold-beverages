cf686103c636688a133054b5fc2bfcd9a1ec3370
shackfu1 <129890135+shackfu1@users.noreply.github.com>
2025-03-16 15:38:12 -0700

    added tests for WaterReservoir

--- spec/water_reservoir_spec.rb
+++ spec/water_reservoir_spec.rb
@@ -1,5 +1,29 @@
 require_relative '../lib/water_reservoir'
 
-describe 'A water reservoir' do
+describe 'Has a capacity and water volume' do
 
+  it 'has a capacity and water volume' do
+    reservoir = WaterReservoir.new(10,5)
+    expect(reservoir.capacity).to eq(10)
+    expect(reservoir.current_water_volume).to eq(5)
+  end
+
+  it 'current water volume is set to capacity when filled' do
+    reservoir = WaterReservoir.new(10,5)
+    reservoir.fill()
+    expect(reservoir.current_water_volume).to eq(reservoir.capacity)
+  end
+
+  it 'is not empty after it is filled' do
+    reservoir = WaterReservoir.new(10,0)
+    expect(reservoir).to be_empty
+    reservoir.fill()
+    expect(reservoir).not_to be_empty
+  end
+
+  it 'loses the correct amount of water when it is drained' do
+    reservoir = WaterReservoir.new(10,8)
+    reservoir.drain(3)
+    expect(reservoir.current_water_volume).to eq(5)
+  end
 end
