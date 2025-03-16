de8348acc82199709f5e30f33df25c4924cc3f0f
shackfu1 <129890135+shackfu1@users.noreply.github.com>
2025-03-16 14:58:44 -0700

    got vessel_spec tests passing

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,17 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :volume, :name
 
   def initialize(name, volume)
     @name = name
     @volume = volume
+    @full = false
   end
 
+  def empty?
+    return not(@full)
+  end
+
+  def fill
+    @full = true
+  end
 end
