6902f4498e5e786f2b2daf7b1f9ba11f49e0cd63
shackfu1 <129890135+shackfu1@users.noreply.github.com>
2025-03-16 15:22:19 -0700

    added tests for WaterDispenser

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -2,4 +2,18 @@ require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
 
+  it 'has a reservoir' do
+    test_reservoir = WaterReservoir.new()
+    dispenser = WaterDispenser.new(test_reservoir)
+    expect(dispenser.reservoir).to be_a(WaterReservoir)
+  end
+
+  it 'decreases its reservoir volume after dispensing a vessel' do
+    test_reservoir = WaterReservoir.new(10,10)
+    dispenser = WaterDispenser.new(test_reservoir)
+    test_vessel = Vessel.new('test', 6)
+    dispenser.dispense(test_vessel)
+    expect(dispenser.reservoir.current_water_volume).to eq(4)
+  end
+
 end
