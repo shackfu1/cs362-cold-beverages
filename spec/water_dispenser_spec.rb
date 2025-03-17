70ff25e0f5217af0693387870bbe434986f89ac7
shackfu1 <129890135+shackfu1@users.noreply.github.com>
2025-03-16 17:16:06 -0700

    added a test for dispense method

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -8,12 +8,21 @@ describe 'A water dispenser' do
     expect(dispenser.reservoir).to be_a(WaterReservoir)
   end
 
-  it 'decreases its reservoir volume after dispensing a vessel' do
+  it 'decreases its reservoir volume after dispensing into a vessel' do
     test_reservoir = WaterReservoir.new(10,10)
     dispenser = WaterDispenser.new(test_reservoir)
     test_vessel = Vessel.new('test', 6)
     dispenser.dispense(test_vessel)
     expect(dispenser.reservoir.current_water_volume).to eq(4)
+  end
+
+  it 'fills a vessel whenever it dispenses' do
+    test_reservoir = WaterReservoir.new(10,10)
+    dispenser = WaterDispenser.new(test_reservoir)
+    test_vessel = Vessel.new('test', 6)
+    expect(test_vessel).to be_empty
+    dispenser.dispense(test_vessel)
+    expect(test_vessel).to_not be_empty
   end
 
 end
